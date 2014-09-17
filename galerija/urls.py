6c854ce8c15cd074fcbeee11cee99eb9c0c0dff9
Marko Seric <seric.marko@gmail.com>
2014-09-17 20:36:03 +0200

    asdasdsads

--- galerija/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-	url(r'^$', 'galerija.views.main', name='main'),
-	url(r'^image/(\d+)/$', 'galerija.views.image', name='image'),
-)
