017191ab370f44e13bc05089756657e39bc70374
dakrizma <dakrizma@gmail.com>
2014-09-19 08:57:05 +0200

    19.9.14

--- galerija/models.py
+++ galerija/models.py
@@ -7,6 +7,8 @@ from jastuk.settings import MEDIA_ROOT
 from django.core.files import File
 from os.path import join
 from tempfile import NamedTemporaryFile
+from cStringIO import StringIO
+from django.core.files.base import ContentFile
 
 class Slika(models.Model):
 	name = models.CharField(max_length=60, blank=True, null=True)
@@ -25,9 +27,14 @@ class Slika(models.Model):
 		im.thumbnail((320,180), Image.ANTIALIAS)
 		thumb_fn = fn + "-thumb" + ext
 		# tf = NamedTemporaryFile()
-		im.save(fn + ext, "JPEG")
+		# im.save(thumb_fn)
 		 # ImageOps.fit(image, size, method, bleed, centering)
-		self.thumbnail.save(thumb_fn, "JPEG")
+		temp_thumb = StringIO()
+		im.save(temp_thumb, "JPEG")
+		temp_thumb.seek(0)
+
+		self.thumbnail.save(thumb_fn, ContentFile(temp_thumb.read()), save=False)
+		temp_thumb.close()
 		# self.thumbnail.save(thumb_fn, File(open(tf.name)), save=False)
 		# tf.close()
 
@@ -48,70 +55,3 @@ class Ocjene(models.Model):
 
 	class Meta:
 		verbose_name_plural = 'ocjene'
-
-
-# from cStringIO import StringIO
-# import os
-
-# from django.db import models
-# from django.core.files.base import ContentFile
-# from django.core.files.storage import default_storage as storage
-
-# from PIL import Image
-
-# # Thumbnail size tuple defined in an app-specific settings module - e.g. (400, 400)
-# from app.settings import THUMB_SIZE
-
-# class Photo(models.Model):
-# 	"""
-# 	Photo model with automatically generated thumbnail.
-# 	"""
-# 	photo = models.ImageField(upload_to='photos')
-# 	thumbnail = models.ImageField(upload_to='thumbs', editable=False)
-
-# 	def save(self, *args, **kwargs):
-# 		"""
-# 		Make and save the thumbnail for the photo here.
-# 		"""
-# 		super(Photo, self).save(*args, **kwargs)
-# 		if not self.make_thumbnail():
-# 			raise Exception('Could not create thumbnail - is the file type valid?')
-
-# 	def make_thumbnail(self):
-# 		"""
-# 		Create and save the thumbnail for the photo (simple resize with PIL).
-# 		"""
-# 		fh = storage.open(self.photo.name, 'r')
-# 		try:
-# 			image = Image.open(fh)
-# 		except:
-# 			return False
-
-# 		image.thumbnail(THUMB_SIZE, Image.ANTIALIAS)
-# 		fh.close()
-
-# 		# Path to save to, name, and extension
-# 		thumb_name, thumb_extension = os.path.splitext(self.photo.name)
-# 		thumb_extension = thumb_extension.lower()
-
-# 		thumb_filename = thumb_name + '_thumb' + thumb_extension
-
-# 		if thumb_extension in ['.jpg', '.jpeg']:
-# 			FTYPE = 'JPEG'
-# 		elif thumb_extension == '.gif':
-# 			FTYPE = 'GIF'
-# 		elif thumb_extension == '.png':
-# 			FTYPE = 'PNG'
-# 		else:
-# 			return False	# Unrecognized file type
-
-# 		# Save thumbnail to in-memory file as StringIO
-# 		temp_thumb = StringIO()
-# 		image.save(temp_thumb, FTYPE)
-# 		temp_thumb.seek(0)
-
-# 		# Load a ContentFile into the thumbnail field so it gets saved
-# 		self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=True)
-# 		temp_thumb.close()
-
-# 		return True
--- galerija/tests.py
+++ galerija/tests.py
@@ -1,1 +1,46 @@
-from django.test import TestCase
+# from django.test import TestCase
+
+from PIL import Image
+
+# test_image = "download.jpg"
+# original = Image.open(test_image)
+# original.show()
+
+# width, height = original.size   # Get dimensions
+# left = width/4
+# top = height/4
+# right = 3 * width/4
+# bottom = 3 * height/4
+# cropped_example = original.crop((left, top, right, bottom))
+
+# cropped_example.show()
+
+
+
+
+pic = Image.open("download.jpg")
+width, height = pic.size
+sirina = 320
+visina = 180
+
+aspect_ratio = width / float(height)
+if aspect_ratio < 1:						# ratio > 1 --> slika je sira
+	aspect_ratio = 1/aspect_ratio
+if (width / sirina > height / visina):		# 
+	final_height = visina
+	final_width = int(visina * aspect_ratio)
+elif (width / sirina < height / visina):
+	final_width = sirina
+	final_height = int(sirina * aspect_ratio)
+else:
+	final_width = sirina
+	final_height = int(sirina * aspect_ratio)
+
+# imaged = pic.resize((final_width, final_height), Image.ANTIALIAS)
+pic.thumbnail((32, 18), Image.ANTIALIAS)
+# left = top = 0
+# right =
+# bottom = visina
+# cropped = imaged.crop((left, top, right, bottom))
+pic.save("thumb.jpg")
+# cropped.save("test.jpg")
